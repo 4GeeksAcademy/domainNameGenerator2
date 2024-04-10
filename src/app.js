92a69af0f7412dddd37e85261c566d7174043b68
José Fidel Paredes Sanchez <98322383+fideviva@users.noreply.github.com>
2024-04-10 16:28:24 +0000

    Exercise

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es"];
+
+  for (let i in pronoun) {
+    for (let t = 0; t < adj.length; t++) {
+      for (let p = 0; p < noun.length; p++) {
+        for (let n of extension) {
+          console.log(pronoun[i] + adj[t] + noun[p] + n);
+        }
+      }
+    }
+  }
 };
